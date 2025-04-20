dd26520bc0153ed63ae5b6269c078e7d24bf9e43
Furkan Uyanık <mehmetfrkn07@gmail.com>
2025-04-20 18:10:26 +0300

    file path

--- backend/src/services/productService.ts
+++ backend/src/services/productService.ts
@@ -6,8 +6,8 @@ import { ProductRaw, ProductResponse } from '../models/Product';
 
 export const getProductsWithPrices = async (): Promise<ProductResponse[]> => {
   const filePath = process.env.NODE_ENV === 'production'
-  ? path.join(__dirname, '..', 'dist', 'data', 'products.json')
-  : path.join(__dirname, '..', 'src', 'data', 'products.json');
+  ? path.join(__dirname, '..', '..', 'dist', 'data', 'products.json')
+  : path.join(__dirname, '..', 'data', 'products.json');
   const rawData = fs.readFileSync(filePath, 'utf-8');
   const products: ProductRaw[] = JSON.parse(rawData);
   const goldPrice = await fetchGoldPrice();
