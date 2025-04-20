c9edadb7495312235cdbf3e6e37630d064e6a2d1
Furkan Uyanık <mehmetfrkn07@gmail.com>
2025-04-20 17:48:01 +0300

    Fixed build script errors.

--- backend/src/services/productService.ts
+++ backend/src/services/productService.ts
@@ -5,7 +5,9 @@ import { calculateProductPrice } from '../utils/calculatePrice';
 import { ProductRaw, ProductResponse } from '../models/Product';
 
 export const getProductsWithPrices = async (): Promise<ProductResponse[]> => {
-  const filePath = path.join(__dirname, '..', 'data', 'products.json');
+  const filePath = process.env.NODE_ENV === 'production'
+  ? path.join(__dirname, '..', 'dist', 'data', 'products.json')
+  : path.join(__dirname, '..', 'src', 'data', 'products.json');
   const rawData = fs.readFileSync(filePath, 'utf-8');
   const products: ProductRaw[] = JSON.parse(rawData);
   const goldPrice = await fetchGoldPrice();
